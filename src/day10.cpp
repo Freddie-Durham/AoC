f8af5062a25feb06d3aac895243ef5f9b10896de
Freddie Daniels <145051079+FredD120@users.noreply.github.com>
2025-12-20 16:00:44 +0000

    first commit from chromebook

---
+++ src/day10.cpp
@@ -0,0 +1,26 @@
+#include "utils.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <unordered_map>
+#include <climits>
+#include <stdint.h>
+using namespace std;
+
+
+
+
+void analyse(string file){
+    vector<vector<long long>> boxes = get_array(file, ',');
+
+}
+
+int main() {
+    string file = "../input/day10_test.txt";
+    analyse(file);
+    return 0;
+}
