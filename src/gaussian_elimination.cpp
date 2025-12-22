f86350bec6ce4ef513e5aa150a3121ece17351b5
Freddie Daniels <145051079+FredD120@users.noreply.github.com>
2025-12-22 23:06:35 +0000

    fixed bug in gaussian elimination, it now works for square matices and solves machines that have the same number of buttons and readouts

--- src/gaussian_elimination.cpp
+++ src/gaussian_elimination.cpp
@@ -1,4 +1,4 @@
-//Failed attempt to solve day 10
+//Failed? attempt to solve day 10
 
 #include "utils.h"
 #include <iostream>
@@ -14,16 +14,30 @@
 #include <bitset>
 using namespace std;
 
+//assume square matrix
+vector<vector<float>> transpose_matrix(vector<vector<float>> &matrix, int columns, int rows){
+    vector<float> new_row(rows, 0.0);
+    vector<vector<float>> new_matrix(columns, new_row);
+    for (int i = 0; i < columns; i++){
+        for (int j = 0; j < rows; j++){
+            new_matrix[i][j] = matrix[j][i];
+        }
+    }
+    return new_matrix;
+}
+
 class Machine{
     public:
         vector <float> target;
         vector<vector<float>> matrix;
+        vector<vector<float>> buttons;
         vector<int> button_order;
         vector<int> state;
         int columns;
         int rows;
 
         Machine(string str){
+            vector<vector<float>> newM;
             for (string sec : split(str, ' ')){
                 if (sec[0] == '['){
                     columns = sec.size() - 2;
@@ -34,7 +48,7 @@ class Machine{
                     for (string num : substr){
                         initial[(stoi(num))] = 1.0;
                     }
-                    matrix.push_back(initial);
+                    newM.push_back(initial);
                 }
                 else if (sec[0] == '{'){
                     vector<string> substr = split(sec.substr(1, sec.size() - 2), ',');
@@ -43,7 +57,7 @@ class Machine{
                     }
                 }
             }
-            rows = matrix.size();
+            rows = newM.size();
             vector<int> initial(columns, 0.0);
             state = initial;
 
@@ -52,6 +66,8 @@ class Machine{
                 pos[i] = i;
             }
             button_order = pos;
+            buttons = newM;
+            matrix = transpose_matrix(newM, columns, rows);
         }
 };
 
@@ -59,8 +75,8 @@ void print_machine(Machine m){
     cout << "Rows = " << m.rows << ", colums = " << m.columns << ". Target: " << "\n";
     print(m.target);
     print(m.matrix);
-    cout << "State:" << "\n";
-    print(m.state);
+    //cout << "State:" << "\n";
+    //print(m.state);
 }
 
 vector<Machine> get_machines(string file){
@@ -83,18 +99,32 @@ vector<Machine> get_machines(string file){
     return array;
 }
 
+void push_button(Machine &m, vector<float> button, int n){
+    //loop through joltage counters, push button n times
+    for (int i; i < m.columns; i++){
+        if (button[i] > 0){
+            m.state[i] += n;
+        }
+    }
+}
+
+void apply_solution(Machine &m){
+    //loop through buttons (they have been re-ordered)
+    for (int i = 0; i < m.rows; i++){
+        push_button(m, m.buttons[i], m.target[i]);
+    }
+}
+
 //swap position of row a and b
-void swap_rows(vector<vector<float>> &matrix, vector<float> &target, vector<int> &buttons, int a, int b){
+void swap_rows(vector<vector<float>> &matrix, vector<float> &target, int a, int b){
     if (a != b){
         swap(matrix[a], matrix[b]);
         swap(target[a], target[b]);
-        swap(buttons[a], buttons[b]);
     }
 }
 
 //multiply row a by v
 void multiply_row(vector<vector<float>> &matrix, vector<float> &target, int a, float v){
-    //could also use: transform(row.begin(), row.end(), row.begin(), [v](float x) { return x * v; });
     for (int i = 0; i < target.size(); i++){
         matrix[a][i] *= v;
     }
@@ -111,11 +141,11 @@ void add_rows(vector<vector<float>> &matrix, vector<float> &target, int a, int b
 
 //find first row where ith column is non-zero and swap that row with ith row
 //return false if there are no rows from i to end where matrix[row][i] != 0
-bool select_row(vector<vector<float>> &matrix, vector<float> &target, vector<int> &buttons, int i){
+bool select_row(vector<vector<float>> &matrix, vector<float> &target, const int &i, const int &rows){
     int largest_index = i;
     float largest_value = 0;
-    for (int row = i; row < target.size(); row++){
-        if (matrix[row][i] > largest_value){
+    for (int row = i; row < rows; row++){
+        if (abs(matrix[row][i]) > largest_value){
             largest_value = matrix[row][i];
             largest_index = row;
         }
@@ -125,80 +155,41 @@ bool select_row(vector<vector<float>> &matrix, vector<float> &target, vector<int
         return false;
     }
     else{
-        swap_rows(matrix, target, buttons, i, largest_index);
+        cout << "Largest index = " << largest_index << "\n";
+        swap_rows(matrix, target, i, largest_index);
         return true;
     }
 }
 
-vector<vector<float>> solve_matrix(vector<vector<float>> matrix, vector<float> &target, vector<int> &buttons){
-    for (int i = 0; i < target.size(); i++){
+void solve_matrix(vector<vector<float>> &matrix, vector<float> &target, const int &rows){
+    for (int i = 0; i < rows; i++){
         //next row must have non-zero value at matrix[i][i]
-        bool found_row = select_row(matrix, target, buttons, i); 
+        bool found_row = select_row(matrix, target, i, rows); 
 
         if (found_row){
             multiply_row(matrix, target, i, 1 / matrix[i][i]);
-            for (int j = 0; j < target.size(); j++){
+            for (int j = 0; j < rows; j++){
                 if (j != i){
                     add_rows(matrix, target, j, i, -matrix[j][i]);
                 }
             }
         }
         print(matrix);
-        cout << "\n";
+        cout << i << "\n";
     }
-    return matrix;
-}
-
-//assume square matrix
-void transpose_matrix(vector<vector<float>> &matrix){
-    for (int i = 0; i < matrix.size(); i++){
-        for (int j = i; j < matrix.size(); j++){
-            swap(matrix[i][j], matrix[j][i]);
-        }
-    }
-}
-
-void push_button(Machine &m, vector<float> button, int n){
-    //loop through joltage counters, push button n times
-    for (int i; i < m.state.size(); i++){
-        if (button[i] > 0){
-            m.state[i] += n;
-        }
-    }
-}
-
-void apply_solution(Machine &m){
-    //loop through buttons (they have been re-ordered)
-    for (int i = 0; i < m.rows; i++){
-        push_button(m, m.matrix[m.button_order[i]], m.target[i]);
-    }
-}
-
-//assume matrix is square
-vector<float> mat_mul(vector<vector<float>> &matrix, vector<float> &vec){
-    vector<float> solution(matrix.size(), 0.0);
-    for (int row = 0; row < matrix.size(); row++){
-        float sum = 0;
-        for (int column = 0; column < matrix.size(); column++){
-            sum += matrix[row][column] * vec[column];
-        }
-        solution[row] = sum;
-    }
-    return solution;
 }
 
 void analyse(string file){
     vector<Machine> machines = get_machines(file);
     long long total = 0;
-    
-    transpose_matrix(machines[1].matrix);
-    vector<vector<float>> solved_matrix = solve_matrix(machines[1].matrix, machines[1].target, machines[1].button_order);
-    //machines[1].target = {2,5,0,5,0};
-    //apply_solution(machines[1]);
-    print_machine(machines[1]);
-    cout << "Solved matrix:" << "\n";
-    print(solved_matrix);
-    //print(mat_mul(machines[1].matrix, machines[1].target));
+
+    for (int i = 0; i < 3; i++){
+        solve_matrix(machines[i].matrix, machines[i].target, machines[i].rows);
+        print_machine(machines[i]);
+
+        //apply_solution(machines[i]);
+        //print(machines[i].state);
+    }
 }
 
 int main() {
